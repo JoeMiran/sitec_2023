79bbb8be0cc968ef83aea54f27a0a00deb8a0640
JoeMiran <joel.miranda@itec.ufpa.br>
2023-09-05 09:40:54 -0300

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   css/style.css 	modified:   index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width", initial-scale="1.0">
         <link rel="stylesheet" type="text/css" href="css/style.css">
-        <link href="http://fonts.cdnfonts.com/css/straightler" rel="stylesheet">
+        <link href="http://fonts.cdnfonts.com/css/low-gun-screen-expanded" rel="stylesheet">
         <title>Início</title>
     </head>
     <body>
