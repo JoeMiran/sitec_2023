b1fa028ae4ec7fc956c706ab719d5fc7717f534c
Caylon Solon <107728284+7Caylon7@users.noreply.github.com>
2023-09-11 11:53:02 -0300

    Add files via upload
    Refatoração da estrutura e da style das paginas.

--- index.php
+++ index.php
@@ -5,56 +5,51 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width", initial-scale="1.0">
-        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="./css/style.css">
         <link href="http://fonts.cdnfonts.com/css/low-gun-screen-expanded" rel="stylesheet">
-        <title>Incio</title>
+        <title>Início</title>
     </head>
     <body>
-        
-        <div class="cabecalho">
-            
-            <div id="area-cabecalho">
+            <div class="area-cabecalho">
                 <div id="area-logo">
                     <h1><span class="roxo">SITEC </span><span class="ano">2023</span></h1>
                 </div>
                 
                 <div id="area-menu">
-                    <a href="">Início</a> 
+                    <a href="index.php">Início</a> 
                     <a href="formulario.php">Formulário</a> 
                     <a href="consulta.php">Consulta</a>  
                 </div>
            
             </div>
-        
-        </div>
 
         <div class="container">
 
-            <!-- Apresenta��o -->
+            <!-- Apresentação -->
             <div class="area_principal">
-                <h2>Semana do ITEC 2023</h2><br>
-                <h4>Desenvolvimento Web Full Stack.</h4><br>
-                <img src="img/itec.png"><br><br>
+                <h2>Semana do ITEC 2023</h2>
+                <h4>Desenvolvimento Web Full Stack.</h4>
+                <img src="img/itec.png">
                 <p>
-                    Esse minicurso tem por finalidade apresentar os conceitos b�sicos de desenvolvimento web full stack,
-                    utilizando as tecnologias HTML, CSS, JavaScript, PHP e MySQL. Nosso objetivo � que ao final do minicurso,
-                    voc� seja capaz de desenvolver um sistema web completo, desde a cria��o do banco de dados at� a interface
-                    de usu�rio. 
+                    Esse minicurso tem por finalidade apresentar os conceitos básicos de desenvolvimento web full stack,
+                    utilizando as tecnologias HTML, CSS, JavaScript, PHP e MySQL. Nosso objetivo é que ao final do minicurso,
+                    você seja capaz de desenvolver um sistema web completo, desde a criação do banco de dados até a interface
+                    de usuário. 
                 </p>   
             </div>
 
             <!-- Login -->
             <main class="area_login">
-                <h2>LOGIN</h2><br><br>
-                <form method="POST" action="index.php">
-                    <input type="text"  name="usuario" placeholder="Usuário" class="inputs" required><br><br>
-                    <input type="password" name="senha" placeholder="Senha" class="inputs" required><br><br>
-                    <span class="erro_login"></span><br>
-                    <div class="botoes">
+                <h2>LOGIN</h2>
+                <form class="formulario" method="POST" action="index.php">
+                    <input type="text"  name="usuario" placeholder="Usuário" class="inputs" required>
+                    <input type="password" name="senha" placeholder="Senha" class="inputs" required>
+                    <span class="erro_login"></span>
+                </form>
+                <div class="botoes">
                         <button class="botao_entrar" type="submit" value="entrar" onclick="consultarConta(event)">Entrar</button>
                         <button class="botao_cadastrar" type="submit" value="cadastrar" onclick="cadastrarConta(event)">Cadastrar</button>
-                    </div>
-                </form>
+                </div>
             </main>
         </div>
 
