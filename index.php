555bef657bb56cf3ee12f5b104246716529b8db4
Caylon Solon <107728284+7Caylon7@users.noreply.github.com>
2023-09-15 10:27:24 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,62 +1,44 @@
-
 <!DOCTYPE html>
 <html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width", initial-scale="1.0">
-        <link rel="stylesheet" type="text/css" href="css/style.css">
-        <link href="http://fonts.cdnfonts.com/css/low-gun-screen-expanded" rel="stylesheet">
-        <title>Início</title>
-    </head>
-    <body>
-        
-        <div class="cabecalho">
-            
-            <div id="area-cabecalho">
-                <div id="area-logo">
-                    <h1><span class="roxo">SITEC </span><span class="ano">2023</span></h1>
-                </div>
-                
-                <div id="area-menu">
-                    <a href="">Início</a> 
-                    <a href="formulario.php">Formulário</a> 
-                    <a href="lista.php">Consulta</a>  
-                </div>
-           
-            </div>
-        
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width" , initial-scale="1.0">
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <link href="http://fonts.cdnfonts.com/css/low-gun-screen-expanded" rel="stylesheet">
+    <title>Início</title>
+</head>
+
+<body>
+    <div class="container"> <!-- Div que envolve todo o conteúdo da página. -->
+        <!-- Apresentação -->
+        <div class="area_principal"> <!--Seção de apresentação com informações básicas -->
+            <h2>Semana do ITEC 2023</h2> <!-- Tag para Título de tamanho 2 -->
+            <h4>Desenvolvimento Web Full Stack.</h4> <!--  Tag para Título de tamanho 4 -->
+            <img src="img/itec.png"> <!-- Tag para adição de imagem -->
+            <p> <!-- Tag paragrafo -->
+                Esse minicurso tem por finalidade apresentar os conceitos básicos de desenvolvimento web full stack,
+                utilizando as tecnologias HTML, CSS, JavaScript, PHP e MySQL. Nosso objetivo é que ao final do minicurso,
+                você seja capaz de desenvolver um sistema web completo, desde a criação do banco de dados até a interface
+                de usuário.
+            </p>
         </div>
 
-        <div class="container">
-
-            <!-- Apresentação -->
-            <div class="area_principal">
-                <h2>Semana do ITEC 2023</h2><br>
-                <h4>Desenvolvimento Web Full Stack.</h4><br>
-                <img src="img/itec.png"><br><br>
-                <p>
-                    Esse minicurso tem por finalidade apresentar os conceitos básicos de desenvolvimento web full stack,
-                    utilizando as tecnologias HTML, CSS, JavaScript, PHP e MySQL. Nosso objetivo é que ao final do minicurso,
-                    você seja capaz de desenvolver um sistema web completo, desde a criação do banco de dados até a interface
-                    de usuário. 
-                </p>   
+        <!-- Seção Login -->
+        <div class="area_login">
+            <h2>LOGIN</h2>
+            <form class="formulario" method="POST" action="index.php"> <!-- Formulário para login que envia dados para "index.php" usando o método POST. -->
+                <input type="text" name="usuario" placeholder="Usuário" class="inputs" required> <!--  Campo de entrada de texto para o nome de usuário. -->
+                <input type="password" name="senha" placeholder="Senha" class="inputs" required> <!--  Campo de entrada de texto para a senha. -->
+                <span class="erro_login"></span> <!--Espaço reservado para exibir mensagens de erro.-->
+            </form>
+            <div class="botoes">
+                <button class="botao_entrar" type="submit" onclick="consultarConta(event)">Entrar</button>
+                <button class="botao_cadastrar" type="submit" onclick="cadastrarConta(event)">Cadastrar</button>
             </div>
-
-            <!-- Login -->
-            <div class="area_login">
-                <h2>LOGIN</h2>
-                <form method="POST" action="login.php">
-                    <input type="text" name="usuario" placeholder="Usuário" class="inputs" required><br><br>
-                    <input type="password" name="senha" placeholder="Senha" class="inputs" required><br><br>
-                    <div class="butoes" style="border: 1px solid red">
-                        <button style="background-color: greenyellow;" type="submit">Entrar</button>
-                        <button class="cadastrar" type="submit">Cadastrar</button>
-                    </div>
-                </form>
-            </div>
-
         </div>
+    </div>
+</body>
 
-    </body>
 </html>
