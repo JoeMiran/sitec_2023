2400cf8f82ead3572e329d26ff49bef5a53b85ec
TacioDaito <taciokikuchi@gmail.com>
2023-09-28 10:48:18 -0300

    .

--- view/login.php
+++ view/login.php
@@ -41,11 +41,11 @@
             <form class="formulario" method="post" action="login.php"> <!-- Formulário para login que envia dados para "index.php" usando o método POST. -->
                 <input type="text" name="login" placeholder="Login" class="inputs" required> <!--  Campo de entrada de texto para o login de usuário. -->
                 <input type="password" name="senha" placeholder="Senha" class="inputs" required> <!--  Campo de entrada de texto para a senha. -->
-                <span class="erro_login"></span> <!--Espaço reservado para exibir mensagens de erro.-->
                 <div class="botoes">
                     <button class="botao_enviar" name="botao_enviar" value="1" type="submit" onclick="validarEntrada(event)">Entrar</button>
                     <a href="cadastro.php" class="botao_cadastrar">Cadastrar</a>
                 </div>
+                <span class="erro_login"></span> <!--Espaço reservado para exibir mensagens de erro.-->
             </form>
         </div>
     </div>
