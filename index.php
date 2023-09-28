9bcc0edc6b0fe02e1a253d6de16cdb2b7f20dd9c
TacioDaito <taciokikuchi@gmail.com>
2023-09-28 09:46:54 -0300

    Espelhamento da etapa1 com etapa2 (index e css)

--- index.php
+++ index.php
@@ -28,15 +28,15 @@
         <!-- Seção Login -->
         <div class="area_login">
             <h2>LOGIN</h2>
-            <form class="formulario" method="POST" action="index.php"> <!-- Formulário para login que envia dados para "index.php" usando o método POST. -->
+            <form class="formulario" method="post" action="index.php"> <!-- Formulário para login que envia dados para "index.php" usando o método POST. -->
                 <input type="text" name="usuario" placeholder="Usuário" class="inputs" required> <!--  Campo de entrada de texto para o nome de usuário. -->
                 <input type="password" name="senha" placeholder="Senha" class="inputs" required> <!--  Campo de entrada de texto para a senha. -->
                 <span class="erro_login"></span> <!--Espaço reservado para exibir mensagens de erro.-->
+                <div class="botoes">
+                    <button class="botao_enviar" type="submit">Entrar</button>
+                    <a href="cadastro.php" class="botao_cadastrar" type="submit">Cadastrar</a>
+                </div>
             </form>
-            <div class="botoes">
-                <button class="botao_entrar" type="submit" onclick="consultarConta(event)">Entrar</button>
-                <button class="botao_cadastrar" type="submit" onclick="cadastrarConta(event)">Cadastrar</button>
-            </div>
         </div>
     </div>
 </body>
