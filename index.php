a79029c6c7fa7639ca21bf1e84149a3bf5275772
TacioDaito <taciokikuchi@gmail.com>
2023-09-28 10:49:19 -0300

    Remoção de texto de validação

--- index.php
+++ index.php
@@ -31,7 +31,6 @@
             <form class="formulario" method="post" action="index.php"> <!-- Formulário para login que envia dados para "index.php" usando o método POST. -->
                 <input type="text" name="usuario" placeholder="Usuário" class="inputs" required> <!--  Campo de entrada de texto para o nome de usuário. -->
                 <input type="password" name="senha" placeholder="Senha" class="inputs" required> <!--  Campo de entrada de texto para a senha. -->
-                <span class="erro_login"></span> <!--Espaço reservado para exibir mensagens de erro.-->
                 <div class="botoes">
                     <button class="botao_enviar" name="botao_enviar" value="1" type="submit" onclick="">Entrar</button>
                     <a href="cadastro.php" class="botao_cadastrar" type="submit">Cadastrar</a>
